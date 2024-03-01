06d6d13b647a81734807b98c668f9bd1473d1e1a
Zeeshan khan <[valid-email]>
2024-03-02 00:10:37 +0500

    first commit

---
+++ question no 6/main.js
@@ -0,0 +1,4 @@
+var personName = "\n \t Muhammad Amir \t \n ";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
---
+++ question no 6/main.ts
@@ -0,0 +1,4 @@
+let personName: string = `\n \t Muhammad Amir \t \n `;
+console.log(personName);
+let stripped : string = personName.trim();
+console.log(stripped);
